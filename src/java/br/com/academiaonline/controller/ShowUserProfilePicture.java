3a6e5eaaceeb6b6c25f27a55c94baeb2267f3d56
Diego C.Sano <diego_ceccarelli@hotmail.com>
2021-03-11 19:36:00 -0300

    Implementa controller para mostrar foto do usuário

---
+++ src/java/br/com/academiaonline/controller/ShowUserProfilePicture.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.academiaonline.controller;
+
+import br.com.academiaonline.DAO.UserDAOImpl;
+import br.com.academiaonline.model.User;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Diego
+ */
+@WebServlet(name = "ShowUserProfilePicture", urlPatterns = {"/ShowUserProfilePicture"})
+public class ShowUserProfilePicture extends HttpServlet {
+
+    private static final int BYTES_DOWNLOAD = 1024;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=ISO-8859-1");
+        int idPerson = Integer.parseInt(request.getParameter("idperson"));
+
+        try {
+            UserDAOImpl dao = new UserDAOImpl();
+            User user = dao.getProfilePicture(idPerson);
+
+            InputStream inputStream = user.getProfilePicture();
+            OutputStream outputStream = response.getOutputStream();
+
+            response.setHeader("Content-Disposition", "attachment; filename = " + user.getId() + ".jpg");
+
+            int read = 0;
+            
+            final byte[] bytes = new byte[BYTES_DOWNLOAD];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+                outputStream.flush();
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Problema ao carregar foto do Cliente! Erro: " + ex.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
